e83fb0143bcc33a172cbb15cff0df04bc517e980
oscar0812 <oscar0812torres@gmail.com>
2017-10-26 13:26:40 -0500

    actual authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
 
   # GET /posts
   # GET /posts.json
@@ -28,7 +29,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to "/posts?u=lvz", notice: 'Post was successfully created.' }
+        format.html { redirect_to "/posts", notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
@@ -42,7 +43,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
-        format.html { redirect_to "/posts/#{@post.id}?u=lvz", notice: 'Post was successfully updated.' }
+        format.html { redirect_to "/posts/#{@post.id}", notice: 'Post was successfully updated.' }
         format.json { render :show, status: :ok, location: @post }
       else
         format.html { render :edit }
@@ -57,7 +58,7 @@ class PostsController < ApplicationController
     puts "hello"
     @post.destroy
     respond_to do |format|
-      format.html { redirect_to "/posts?u=lvz", notice: 'Post was successfully deleted.' }
+      format.html { redirect_to "/posts", notice: 'Post was successfully deleted.' }
       format.json { head :no_content }
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :posts
   get 'post/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => "posts#index"
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
 end
